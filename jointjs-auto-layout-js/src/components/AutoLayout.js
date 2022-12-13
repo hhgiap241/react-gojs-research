4a253f09d23d85b9be746846fb1553ab1e0ceca2
hoanghuugiap241 <hoanghuugiap241@gmail.com>
2022-12-13 10:01:04 +0700

    Add directed graph with auto layout

---
+++ jointjs-auto-layout-js/src/components/AutoLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AutoLayout = () => {
+  return (
+      <div>
+
+      </div>
+  );
+};
+
+export default AutoLayout;
--- jointjs-auto-layout/src/components/AutoLayout.tsx
+++ jointjs-auto-layout/src/components/AutoLayout.tsx
@@ -1,26 +1,26 @@
-import React, {useEffect, useRef} from 'react';
-// @ts-ignore
 import dagre from 'dagre';
-// @ts-ignore
 import graphlib from 'graphlib';
-import {dia, shapes} from 'jointjs';
+import * as joint from 'jointjs';
+import React, {useEffect, useRef} from 'react';
 import './AutoLayout.css';
-import * as joint from "jointjs";
-
+import {dia} from "jointjs";
 const AutoLayout = (): JSX.Element => {
+  const graphRef = useRef<joint.dia.Graph | null>(null);
   const canvas: any = useRef(null);
   useEffect(() => {
-    const graph = new dia.Graph({}, {cellNamespace: shapes});
-    const paper = new dia.Paper({
+    const graph = new joint.dia.Graph({}, {cellNamespace: joint.shapes});
+    graphRef.current = graph;
+    const paper = new joint.dia.Paper({
       model: graph,
       background: {
         color: '#F8F9FA',
       },
       frozen: true,
       async: true,
-      sorting: dia.Paper.sorting.APPROX,
-      cellViewNamespace: shapes
+      sorting: joint.dia.Paper.sorting.APPROX,
+      cellViewNamespace: joint.shapes
     });
+
 
     var rect = new joint.shapes.standard.Rectangle();
     rect.position(100, 30);
@@ -56,6 +56,11 @@ const AutoLayout = (): JSX.Element => {
     link2.target(rect3);
     link2.addTo(graph);
 
+    joint.layout.DirectedGraph.layout(graph, {
+      graphlib: graphlib,
+      dagre: dagre,
+      setLinkVertices: true,
+    });
 
     // graph.addCell(rect);
     paper.unfreeze();
@@ -69,6 +74,13 @@ const AutoLayout = (): JSX.Element => {
 
   const handleAutoLayout = () => {
     console.log('Auto Layout');
+    if (graphRef.current) {
+      joint.layout.DirectedGraph.layout(graphRef.current, {
+        graphlib: graphlib,
+        dagre: dagre,
+        setLinkVertices: true,
+      });
+    }
   }
 
   return (
